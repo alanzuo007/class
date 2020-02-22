ebc8951fbd36f7f0666d932162ca9e11df748a40
alanzuo007 <35772139+alanzuo007@users.noreply.github.com>
2020-02-22 15:07:58 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,9 @@
+# from wsgiref.simple_server import make_server
+#
+# import os
+# print(os.getcwd())
+#
+# from manage import main
+# httpd=make_server('',8000,main())
+# print('serving http on port 8000...')
+# httpd.serve_forever()
